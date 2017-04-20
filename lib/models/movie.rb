db1dd9dbdeac6b4587c012759fbc96e25c37bc56
ipc103 <iancandysemail@gmail.com>
2017-04-20 12:20:04 -0400

    more methods

--- lib/models/movie.rb
+++ lib/models/movie.rb
@@ -4,15 +4,15 @@ class Movie < ActiveRecord::Base
   has_many :categories, through: :movie_categories
 
   def self.average_rating
-    # Find me the average rating for ALL of the movies, not just one particular movie
+    self.joins(:reviews).average(:rating).to_f
   end
 
   def self.eighties
-    # Find me all of the movies that have a release year between 1980 and 1989
+    self.where(release_year: (1980..1989) )
   end
 
   def self.average_rating_eighties_movies
-    # this should return the average rating for all of the moveis that were released between 1980 and 1989
+    self.eighties.average_rating
   end
 
   def self.highest_rated
