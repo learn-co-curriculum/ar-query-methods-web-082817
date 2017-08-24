99c3de6eebfc0635a0ae1bc682348674d58d3559
Alex Griffith <alex.griffith@flatironschool.com>
2017-08-24 14:38:30 -0400

    reset code

--- lib/models/movie.rb
+++ lib/models/movie.rb
@@ -4,15 +4,15 @@ class Movie < ActiveRecord::Base
   has_many :categories, through: :movie_categories
 
   def self.average_rating
-    self.joins(:reviews).average(:rating).to_f
+    
   end
 
   def self.eighties
-    self.where(release_year: (1980..1989) )
+    
   end
 
   def self.average_rating_eighties_movies
-    self.eighties.average_rating
+    
   end
 
   def self.highest_rated
@@ -27,11 +27,11 @@ class Movie < ActiveRecord::Base
   end
 
   def average_rating
-    self.reviews.average(:rating).to_f
+    
   end
 
   def number_of_reviews
-    self.reviews.count
+    
   end
 
 end
